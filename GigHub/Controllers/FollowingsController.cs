f61958cd4998e052756eb6a5bda41fca6feea421
Hasin <hasin.ajaz1996@hotmail.com>
2019-02-23 18:31:43 +0000

    added following js to fulfill api

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,45 @@
+﻿
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Follow([FromBody] FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Already following");
+
+            Following following = new Following
+            {
+                FolloweeId = dto.FolloweeId,
+                FollowerId = userId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
+    }
+}
