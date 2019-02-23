9b24f998c452fd13b9271072ea087b1237fd465a
Hasin <hasin.ajaz1996@hotmail.com>
2019-02-23 18:17:12 +0000

    Fixed viewModel bug, added followings domain model

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] AttendanceDTO dto)
+        {
+            var user = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(g => g.GigId == dto.GigId && g.AttendeeId == user))
+                return BadRequest("The attendance already exists");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = user
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -7,6 +7,8 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
 
 
         public ApplicationDbContext()
@@ -17,6 +19,27 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(u => u.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(u => u.Follower)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
