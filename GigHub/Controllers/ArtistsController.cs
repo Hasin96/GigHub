edef86a3673228ec0b157d57e267dcd8d624361a
Hasin <hasin.ajaz1996@hotmail.com>
2019-02-23 19:20:28 +0000

    Implenting a page where user can see the artists they're following

---
+++ GigHub/Controllers/ArtistsController.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Followees()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var followees = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(followees);
+        }
+    }
+}
